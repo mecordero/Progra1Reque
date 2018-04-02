b11f10c80475494ac40ac4f253f6475500db4c44
FA250 <faph.2009@hotmail.com>
2018-04-02 17:18:28 -0600

    0.2

---
+++ pruebasBD.sql
@@ -0,0 +1,23 @@
+USE [BD_Baratico]
+GO
+insert into CLIENTE values ('Fabián','Piedra Herrera','faph.2009@hotmail.com','Cartago')
+insert into Administrador values ('Fabián','Piedra Herrera')
+insert into Log_In values ('CliF','CliF',2,1),('AdF','AdF',1,1)
+
+insert into CATEGORIA values ('Limpieza'),('Comida'), ('Hogar'),('Ropa'),('Ferreteria')
+insert into PRODUCTO values ('Escoba',1,0,2200,0), ('Jabón',1,0,850,0), ('Arroz',2,0,1100,0), ('Frijoles',2,0,1150,0), ('Alfombra',3,0,3200,0), ('Mesa',3,0,4200,0), ('Camisa azul',4,0,3400,0), ('Jeans',4,0,2450,0), ('Clavo',5,0,50,0),('Martillo',5,0,1850,0)
+
+select * from cliente 
+select * from administrador 
+select * from LOG_IN 
+
+select * from CATEGORIA
+select * from PRODUCTO
+
+delete LOG_IN where idLogIn != 1 and idLogIn!=2
+delete CLIENTE where NOMBRE != 'Fabián'
+delete Administrador where NOMBRE != 'Fabián'
+
+USE [master]
+GO
+drop database BD_Baratico
--- sqlProcedures.sql
+++ sqlProcedures.sql
@@ -1,7 +1,10 @@
+USE [BD_Baratico]
+GO
+
 CREATE PROCEDURE Verificacion_LogIn @Usuario nchar(15)
 AS
 BEGIN
-	select Usuario,Clave,tipo,iddueño from LOG_IN where usuario=@usuario
+	select Usuario,Clave,tipo,iddueño from LOG_IN where Usuario=@usuario
 END
 GO
 
@@ -16,5 +19,36 @@ CREATE PROCEDURE Obtener_Admin @id int
 AS
 BEGIN
 	select Nombre,Apellidos from Administrador where IdAdiministrador=@id
+END
+GO
+
+CREATE PROCEDURE Buscar_Usuario @Usuario nchar(15)
+AS
+BEGIN
+	select Usuario from LOG_IN where usuario=@usuario
+END
+GO
+
+
+CREATE PROCEDURE Registrar_Cliente @nombre nchar(20),@apellidos nchar(40),@correo nchar(50), @usuario nchar(15), @clave nchar(20)
+AS
+BEGIN	
+	insert into CLIENTE values (@nombre,@apellidos,@correo,null)
+	insert into Log_In values (@usuario,@clave,2,(SELECT TOP 1 idCliente FROM cliente cl ORDER BY cl.idCliente DESC))
+END
+GO
+
+CREATE PROCEDURE Obtener_Productos
+AS
+BEGIN	
+	select IdProducto,Nombre,idCategoría,CantidadVendida,Precio,CalificacionPromedio from producto
+END
+GO
+
+
+CREATE PROCEDURE Obtener_Categorias
+AS
+BEGIN	
+	select IdCategoría,Nombre from categoria
 END
 GO
